ae2d3efe8f2939debbe3a4a1bcdfdcbf440492b8
wanjiku5 <awanjikunjeri@gmail.com>
2024-02-26 13:12:35 +0300

    initial commit

--- app.py
+++ app.py
@@ -2,4 +2,8 @@
 
 
 
-print("Hello World")
+name = "Ann Njeri"
+number = 1
+
+bool = True
+print(type(name))
---
+++ user-input.py
@@ -0,0 +1,12 @@
+
+
+
+
+
+name =input("What is you name? ")
+age =input("How old are you? ")
+location = input("Where do you stay? " )
+
+message = f"My name is {name}, I am {age} years old from {location}"
+print(message)
+
